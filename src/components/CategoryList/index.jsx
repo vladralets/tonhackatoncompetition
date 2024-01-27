013020a601bb38dc006e1971d1e6006acd04f309
Vladyslav Ralets <vladralets@gmail.com>
2024-01-27 21:50:45 +0100

    init

--- src/components/CategoryList/index.jsx
+++ src/components/CategoryList/index.jsx
@@ -1,10 +1,8 @@
 import style from './style.module.scss'
 
-const tg = window.Telegram.WebApp
+
 const categories = ['Vše', 'Snídaně', 'Bagely', 'Saláty', 'Snacky', 'Polévky', 'Dezerty', 'Nápoje', 'Káva'];
-if (tg.platform === "macos" || tg.platform === "windows") {
-  style.category["&::-webkit-scrollbar"] = { display: "block" };
-}
+
 
 const CategoryList = ({
 	// eslint-disable-next-line react/prop-types
@@ -17,9 +15,6 @@ const CategoryList = ({
 					key={index} 
 					onClick={() => onCategoryClick(category)} 
 					className={style.category__button}
-					style={{
-						
-					}}
 				>
 					{category}
 				</button>
