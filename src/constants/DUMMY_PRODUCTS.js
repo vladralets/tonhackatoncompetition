bb0721dc2ff8e24be21793387890f3e36877e5a6
Vladyslav Ralets <vladralets@gmail.com>
2024-01-28 18:33:49 +0100

    gogi

--- src/constants/DUMMY_PRODUCTS.js
+++ src/constants/DUMMY_PRODUCTS.js
@@ -8,21 +8,21 @@ export const DUMMY_PRODUCTS = [
 					products: [
 						{
 							id: 1,
-							name: 'Shaurma 1',
+							name: 'SPECIAL GOGIES BREAKFAST',
 							price: 100,
 							image: '/images/products/buritto.jpeg',
 							description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 						},
 						{
 							id: 2,
-							name: 'Shaurma 2',
+							name: 'BENEDICT BREAKFAST',
 							price: 100,
 							image: '/images/products/gogi.jpeg',
 							description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 						},
 						{
 							id: 3,
-							name: 'Shaurma 3',
+							name: 'AVOCADO SPECIAL',
 							price: 100,
 							image: '/images/products/gogi.jpeg',
 							description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
@@ -35,21 +35,21 @@ export const DUMMY_PRODUCTS = [
 					products: [
 						{
 							id: 4,
-							name: 'Buritto L',
+							name: 'DUCK SPECIAL 1',
 							price: 100,
 							image: '/images/products/shaurma.jpeg',
 							description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 						},
 						{
 							id: 5,
-							name: 'Buritto XL',
+							name: 'DUCK VERY SPECIAL',
 							price: 120,
 							image: '/images/products/shaurma.jpeg',
 							description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 						},
 						{
 							id: 6,
-							name: 'Buritto King Size',
+							name: 'DUCK SPECIAL 2',
 							price: 100,
 							image: '/images/products/shaurma.jpeg',
 							description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
